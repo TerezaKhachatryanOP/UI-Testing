56be1a16de9cd4013a3c823b7e4bc6282214ff4b
Tereza Khachatryan <Tereza.Khachatryan@hearst.com>
2026-03-18 16:19:24 +0400

    Register with invalid data

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -19,3 +19,26 @@ test('Register with valid data', async ({ page }) => {
   await page.locator('.action.submit.primary').click()
   await expect(page).toHaveURL(/account/);
 })
+
+// Verify system prevents registration with invalid email
+test('Register with invalid data', async ({ page }) => {
+  await page.goto('https://dashboard.mageplaza.com/customer/account/create/')
+
+  await page.locator('input[name="firstname"]').fill('Teressaa')
+  await page.locator('input[name="lastname"]').fill('')
+  await page.locator('input[name="email"]').fill('test.com')
+  await page.locator('#password').fill('superSecretPassword123!')
+  await page.locator('input[name="password_confirmation"]').fill('otherPassword!')
+
+  await expect(page.locator('input[name="firstname"]')).toHaveValue('Teressaa');
+  await expect(page.locator('input[name="lastname"]')).toHaveValue('');
+  await expect(page.locator('input[name="email"]')).toHaveValue('test.com');
+  await expect(page.locator('#password')).toHaveValue('superSecretPassword123!');
+  await expect(page.locator('input[name="password_confirmation"]')).toHaveValue('otherPassword!');
+
+  await page.locator('.action.submit.primary').click()
+
+  await expect(page.locator('#lastname-error')).toHaveText('This is a required field.');
+  await expect(page.locator('#email_address-error')).toHaveText(/valid email/i);
+  await expect(page.locator('#password-confirmation-error')).toHaveText('Please enter the same value again.');
+})
